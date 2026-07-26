aa4a6f4bf1d9e08255d6325cd8e8ab01e3b7519d
janar <janardhanp008@gmail.com>
2026-07-26 23:29:26 +0530

    Add arrays chapter and update loops files

---
+++ Chapter_09_Arrays/01_Arrays.js
@@ -0,0 +1,4 @@
+let arr = [10, 20, 30, 40];
+console.log(arr.length);
+// 0 to 3
+console.log(arr[4]);
---
+++ Chapter_09_Arrays/03_Array_Modification.js
@@ -0,0 +1,9 @@
+// Accessing & Modifying
+
+let statuses = ["pass", "fail", "skip"];
+
+console.log(statuses[0]);
+console.log(statuses[2]);
+
+
+console.log(statuses.at(-1));
